647bb353cd403d9ef97bbdd47f2a6bb21a63a3d7
lwj <214245088@qq.com>
2018-07-23 17:09:56 +0800

    111

--- src/common/js/jsonp.js
+++ src/common/js/jsonp.js
@@ -1,8 +1,7 @@
 import originJsonp from 'jsonp'
 
-export default function jsonp(url, data, option) {
-  url += (url.indexOf('?') < 0 ? '?' : '&') + param(data)
-
+export default function jsonp (url, data, option) {
+  url += url.indexOf('?') < 0 ? '?' : '&' + param(data)
   return new Promise((resolve, reject) => {
     originJsonp(url, option, (err, data) => {
       if (!err) {
@@ -14,11 +13,10 @@ export default function jsonp(url, data, option) {
   })
 }
 
-export function param(data) {
+function param (data) {
   let url = ''
-  for (var k in data) {
-    let value = data[k] !== undefined ? data[k] : ''
-    url += '&' + k + '=' + encodeURIComponent(value)
+  for (let k in data) {
+    url += `&${k}=${encodeURIComponent(data[k])}`
   }
-  return url ? url.substring(1) : ''
+  return url.substring(1)
 }
