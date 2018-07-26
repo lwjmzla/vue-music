47812be7a26041182c2c233e2c6a018caf58f520
lwj <214245088@qq.com>
2018-07-26 18:06:36 +0800

    1111111

---
+++ src/base/listview/listview.vue
@@ -0,0 +1,156 @@
+<template>
+  <div  class="listview" ref="listview">
+    <ul ref="listview_scroll">
+      <li class="list-group listAlpha" ref="hot" >
+        <h2 class="list-group-title">热门</h2>
+        <ul>
+          <li v-for="(item,index) in hotSingers" class="list-group-item" :key="index">
+            <img class="avatar" :src="'https://y.gtimg.cn/music/photo_new/T001R300x300M000'+item.Fsinger_mid+'.jpg?max_age=2592000'">
+            <span class="name">{{item.Fsinger_name}}</span>
+          </li>
+        </ul>
+      </li>
+      <li v-for="(group,index) in normalSingers" class="list-group listAlpha" :ref="group.title" :key="index">
+        <h2 class="list-group-title">{{group.title}}</h2>
+        <ul>
+          <li v-for="(item,index) in group.items" class="list-group-item" :key="index">
+            <img class="avatar" v-lazy="'https://y.gtimg.cn/music/photo_new/T001R300x300M000'+item.Fsinger_mid+'.jpg?max_age=2592000'">
+            <span class="name">{{item.Fsinger_name}}</span>
+          </li>
+        </ul>
+      </li>
+    </ul>
+    <div class="list-shortcut" @touchstart="onShortcutTouchStart" @touchmove="onShortcutTouchMove" @touchend="onShortcutTouchEnd" ref="">
+      <ul ref="alphaUl">
+        <li class="item" :class="{'current':currentIndex===index}" @click="handleLetterClick">热</li>
+        <li v-for="(item, index) in normalSingers" class="item" :class="{'current':currentIndex===index}" @click="handleLetterClick"  :key="index">{{item.title}}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+import BScroll from 'better-scroll'
+// import Loading from 'base/loading/loading'
+// import {getData} from 'common/js/dom'
+
+// const TITLE_HEIGHT = 30
+// const ANCHOR_HEIGHT = 18
+
+export default {
+  props: {
+    hotSingers: Array,
+    normalSingers: Array
+  },
+  data () {
+    return {
+      currentIndex: 0
+    }
+  },
+  mounted () {
+    this.scroll = new BScroll(this.$refs.listview)
+    // console.dir(this.$refs.listview_scroll)
+    // this.startY = this.$refs['A'][0].offsetTop
+    this.ulToTop = this.$refs.alphaUl.offsetTop
+  },
+  methods: {
+    handleLetterClick (e) {
+      // console.log(e.target.innerText)
+      let letter = e.target.innerText
+      if (letter === '热') {
+        letter = 'hot'
+      }
+      this.scroll.scrollToElement(this.$refs[letter][0] || this.$refs[letter])
+    },
+    onShortcutTouchStart (e) {
+      this.touchStatus = true
+    },
+    onShortcutTouchMove (e) {
+      if (this.touchStatus) {
+        const touchY = e.touches[0].clientY - this.ulToTop - 20 - 88 // 相当于是基于开始点 长度多少
+        const index = Math.floor(touchY / 18)
+        // console.log(index)
+        // console.log(document.querySelectorAll('.listAlpha')[index])
+        this.currentIndex = index
+        this.scroll.scrollToElement(document.querySelectorAll('.listAlpha')[index])
+      }
+    },
+    onShortcutTouchEnd (e) {
+      this.touchStatus = false
+    }
+  }
+}
+
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+
+  .listview
+    position: relative
+    width: 100%
+    height: 100%
+    overflow: hidden
+    background: $color-background
+    .list-group
+      padding-bottom: 30px
+      .list-group-title
+        height: 30px
+        line-height: 30px
+        padding-left: 20px
+        font-size: $font-size-small
+        color: $color-text-l
+        background: $color-highlight-background
+      .list-group-item
+        display: flex
+        align-items: center
+        padding: 20px 0 0 30px
+        .avatar
+          width: 50px
+          height: 50px
+          border-radius: 50%
+        .name
+          margin-left: 20px
+          color: $color-text-l
+          font-size: $font-size-medium
+    .list-shortcut
+      position: absolute
+      z-index: 30
+      right: 0
+      top: 0
+      width: 20px
+      height: 100%
+      text-align: center
+      font-family: Helvetica
+      display: flex
+      justify-content :center
+      flex-direction :column
+      ul
+        background: $color-background-d
+        padding: 20px 0
+        border-radius: 10px
+      .item
+        padding: 3px
+        line-height: 1
+        color: $color-text-l
+        font-size: $font-size-small
+        &.current
+          color: $color-theme
+    .list-fixed
+      position: absolute
+      top: 0
+      left: 0
+      width: 100%
+      .fixed-title
+        height: 30px
+        line-height: 30px
+        padding-left: 20px
+        font-size: $font-size-small
+        color: $color-text-l
+        background: $color-highlight-background
+    .loading-container
+      position: absolute
+      width: 100%
+      top: 50%
+      transform: translateY(-50%)
+</style>
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,22 +1,79 @@
 <template>
-  <div>
-    歌手
+  <div class="singer" ref="singer">
+    <list-view  v-if="singers.length" :hotSingers="hotSingers" :normalSingers="normalSingers" ref="list"></list-view>
   </div>
 </template>
 
 <script>
+import {ERR_OK} from 'api/config'
+import axios from 'axios'
+import ListView from 'base/listview/listview'
 export default {
   data () {
     return {
-
+      singers: []
+    }
+  },
+  computed: {
+    hotSingers () {
+      let arr = []
+      arr = this.singers.filter((obj, index) => {
+        return index <= 9
+      })
+      return arr
+    },
+    normalSingers () {
+      let obj = {}
+      this.singers.forEach((value, index) => {
+        if (!obj[value.Findex]) {
+          obj[value.Findex] = {
+            title: value.Findex,
+            items: []
+          }
+        }
+        let subObj = obj[value.Findex]
+        subObj.items.push(value)
+      })
+      if (obj[9]) {
+        delete obj[9]
+      }
+      // console.log(obj)
+      // 排序数据
+      let arr = []
+      for (let key in obj) {
+        arr.push(obj[key])
+      }
+      arr.sort((a, b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+      })
+      return arr
     }
   },
   components: {
-
+    ListView
+  },
+  created () {
+    this._getSingerList()
+  },
+  methods: {
+    _getSingerList () {
+      axios.get('/api/singer.json')
+        .then((resp) => {
+          const res = resp.data
+          // console.log(res)
+          if (res.code === ERR_OK) {
+            this.singers = res.data.list
+          }
+        })
+    }
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-
+  .singer
+    position: fixed
+    top: 88px
+    bottom: 0
+    width: 100%
 </style>
